e47a22b1a1f4d415a5790e382545ba096c454eeb
raysiagian <anggasiagian17@gmail.com>
2024-05-10 15:31:48 +0700

    hapus level pada unit model

--- lib/models/unit/unit.dart
+++ lib/models/unit/unit.dart
@@ -1,20 +1,19 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:mathgasing/core/constants/constants.dart';
-import 'package:mathgasing/models/level/level.dart';
+
 
 class Unit {
   final int id_unit;
   final String title;
   final String explanation;
-  final List<Level> levels;
-  
+
 
   Unit({
     required this.id_unit,
     required this.title,
     required this.explanation,
-    required this.levels,
+
   });
 
   factory Unit.fromJson(Map<String, dynamic> json) {
@@ -22,7 +21,6 @@ class Unit {
       id_unit: json["id_unit"] as int,
       title: json["title"] as String,
       explanation: json["explanation"] as String,
-      levels: [],
     );
   }
 
